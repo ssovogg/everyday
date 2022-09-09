16ca443dd4610ea23f49248a7ff3203024345ff6
ssovogg <susuco77@gmail.com>
2022-09-09 11:07:58 +0900

    add header

---
+++ src/Layout/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import classes from "./Header.module.css";
+
+const Header = ({ auth }) => {
+  const [toggleProfle, setToggleProfle] = useState(false);
+  const [toggleLogout, setToggleLogout] = useState(false);
+  const onLogout = () => auth.logout();
+  const toggleProfileBtn = () => setToggleProfle((prev) => !prev);
+  const toggleLogouteBtn = () => setToggleLogout((prev) => !prev);
+
+  return (
+    <header className={classes.header}>
+      <h1 className={classes.title}>Everday Routine</h1>
+      <ul className={classes.nav}>
+        <li onMouseEnter={toggleProfileBtn} onMouseLeave={toggleProfileBtn}>
+          {!toggleProfle ? (
+            <div>
+              <i class="fa-solid fa-user"></i>
+            </div>
+          ) : (
+            <span>profile</span>
+          )}
+        </li>
+        <li onMouseEnter={toggleLogouteBtn} onMouseLeave={toggleLogouteBtn}>
+          <button onClick={onLogout}>
+            {!toggleLogout ? (
+              <div>
+                <i class="fa-solid fa-right-from-bracket"></i>
+              </div>
+            ) : (
+              <sapn>Logout</sapn>
+            )}
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import classes from './Home.module.css';
+import React from "react";
+import Header from "../../Layout/Header/Header";
+import classes from "./Home.module.css";
 
 const Home = ({ auth }) => {
-  const onLogout = () => auth.logout();
   return (
-  <>
-    <h1>Home</h1>
-    <button onClick={onLogout}>Logout</button>
-  </>
-  )};
+    <div className={classes.wrap}>
+      <Header auth={auth}/>
+      <section>
+
+      </section>
+    </div>
+  );
+};
 
 export default Home;
